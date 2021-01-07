d1c8059ed605be4e7d7bb2820ec1c83f9d498dd7
Arthur Korchkov <qaeng16@gmail.com>
2021-01-07 11:21:35 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+  //problem1
+  /*
+  string day;
+  cout << " Please, enter a day: " << endl;
+  cin >> day;
+    if ((day == "Tuesday") || (day == "tuesday" || day == "Thursday") || (day == "thursday")) {
+      cout << "You have a CS111 class today. " << endl;
+    } else{
+      cout << "No CS111 today! ";
+    }
+         //problem2
+  int n;
+  cout << "What is n?";
+  cout << endl;
+  cin >> n;
+  cout << endl;
+  for (int r = 1; r <= (2*n-1); r++) {
+    for (int c = 1; c <= n; c++) {
+      if (c <= r && c <= 2 * n - r) {
+      cout << "*";
+      }
+    }
+    cout << endl;
+  }
+  
+
+
+  // problem 3  
+  
+    int x = 6, y = 0, z = 3;
+    double a = 5.0, b = 4.0;
+   // for (; x<=9; x++){
+      cout << x << endl;
+      cout << 4.0 / 6 *3;
+      */
+      int main() {
+      return 0;
+}
+   
